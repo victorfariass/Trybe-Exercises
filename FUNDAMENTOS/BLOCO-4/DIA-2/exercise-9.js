5388e6036632bf70f5a4df35e4f1d52786188899
victor Farias <victorfarias.tv@gmail.com>
2020-10-26 16:08:25 -0300

    Atualizando exercicio 9

--- FUNDAMENTOS/BLOCO-4/DIA-2/exercise-9.js
+++ FUNDAMENTOS/BLOCO-4/DIA-2/exercise-9.js
@@ -1,4 +1,4 @@
 let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
 for (pos=0; pos < numbers.length; pos++) {
-  console.log(numbers[pos]*2)
+  console.log(numbers[pos]/2)
 }
