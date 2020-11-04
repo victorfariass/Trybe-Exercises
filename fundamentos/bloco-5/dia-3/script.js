4f35038728da1a3b54022168d6a08d4656cdaeb8
victor Farias <victorfarias.tv@gmail.com>
2020-11-04 15:30:26 -0300

    Adicionado exercicio 1

--- fundamentos/bloco-5/dia-3/script.js
+++ fundamentos/bloco-5/dia-3/script.js
@@ -14,3 +14,26 @@ function createDaysOfTheWeek() {
 createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
+
+const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+let getDezDays = document.querySelector('#days');
+for (index = 0; index < dezDaysList.length; index += 1) {
+  let day = dezDaysList[index];
+
+  let dayItem = document.createElement('li');
+  if (day === 24 || day === 25 || day === 31) {
+    dayItem.className = "day holiday";
+    dayItem.innerHTML = day;
+    getDezDays.appendChild(dayItem);
+  } else if (day === 4 || day === 11 || day === 18 || day === 25) {
+    dayItem.className = "day friday";
+    dayItem.innerHTML = day;
+    getDezDays.appendChild(dayItem);
+  } else {
+    dayItem.className = "day";
+    dayItem.innerHTML = day;
+    getDezDays.appendChild(dayItem);
+  }
+}
+
