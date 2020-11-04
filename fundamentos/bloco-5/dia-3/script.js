3262873be81781ea62eba84c83971960fbe079d4
victor Farias <victorfarias.tv@gmail.com>
2020-11-04 19:33:09 -0300

    Adicionado exercicio 4

--- fundamentos/bloco-5/dia-3/script.js
+++ fundamentos/bloco-5/dia-3/script.js
@@ -15,6 +15,7 @@ createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
 
+// Exercício 1
 const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
 let getDezDays = document.querySelector('#days');
@@ -41,15 +42,17 @@ for (index = 0; index < dezDaysList.length; index += 1) {
   }
 }
 
-function createButton(buttonName) {
+// Exercício 2
+function createButtonHoliday(buttonName) {
   let buttonContainer = document.querySelector('.buttons-container');
   let buttonHoliday = document.createElement('button');
   buttonHoliday.id = "btn-holiday";
   buttonHoliday.innerHTML = buttonName;
   buttonContainer.appendChild(buttonHoliday);
 }
-createButton("Feriados");
+createButtonHoliday("Feriados");
 
+// Exercício 3
 function colorHoliday() {
   let buttonHoliday = document.querySelector('#btn-holiday');
   let holidays = document.querySelectorAll('.holiday');
@@ -67,3 +70,15 @@ function colorHoliday() {
   })
 };
 colorHoliday();
+
+// Exercício 4
+function createButtonFriday(buttonName) {
+  let buttonContainer = document.querySelector('.buttons-container');
+  let buttonFriday = document.createElement('button');
+  buttonFriday.id = "btn-friday";
+  buttonFriday.innerHTML = buttonName;
+  buttonContainer.appendChild(buttonFriday);
+}
+createButtonFriday("Sexta-feira");
+
+// Exercício 5
