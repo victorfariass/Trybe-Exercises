a45b7b8888e8f3b3c619e826b40d7e459728ffdf
victor Farias <victorfarias.tv@gmail.com>
2020-11-06 10:39:36 -0300

    Reiniciando projeto

--- fundamentos/bloco-5/dia-3/script.js
+++ fundamentos/bloco-5/dia-3/script.js
@@ -128,3 +128,13 @@ function adicionaTarefa(string) {
 adicionaTarefa('Estudar');
 
 // Exercício 8
+function backgroundColor(color) {
+  let taskContainer = document.querySelector('.my-tasks');
+  let newTask = document.createElement('div');
+  newTask.className = 'task';
+  newTask.style.backgroundColor = color;
+  taskContainer.appendChild(newTask);
+}
+backgroundColor('red')
+
+// Exercício 9
