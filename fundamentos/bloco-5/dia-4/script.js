efc15e0cef359e532e3613661a13e827d76f9052
victor Farias <victorfarias.tv@gmail.com>
2020-11-07 12:35:42 -0300

    Adicionado personalizaçao de backgroundColor, fontColor, fontHeight e fontFamily

---
+++ fundamentos/bloco-5/dia-4/script.js
@@ -0,0 +1,63 @@
+window.onload = function() {
+  // Background Color
+  function mudaBackgroundColor(color) {
+    let content = document.querySelector('.content');
+    content.style.backgroundColor = color;
+    localStorage.setItem('backgroundColor', color);
+  }
+
+  let backgroundColorButtons = document.querySelectorAll('#background-color>button');
+  for (let i = 0; i < backgroundColorButtons.length; i += 1) {
+    backgroundColorButtons[i].addEventListener('click', function(event) {
+      mudaBackgroundColor(event.target.innerHTML);
+    })
+  }
+
+  // Font Color
+  function mudaFontColor(color) {
+    let paragraphs = document.querySelectorAll('.paragraph')
+    for (let i = 0; i < paragraphs.length; i += 1) {
+      paragraphs[i].style.color = color;
+    }
+    localStorage.setItem('fontColor', color);
+  }
+
+  let fontColorButtons = document.querySelectorAll('#font-color>button');
+  for (let i = 0; i < fontColorButtons.length; i += 1) {
+    fontColorButtons[i].addEventListener('click', function(event){
+      mudaFontColor(event.target.innerHTML);
+    })
+  }
+
+  // Line Hight
+  function mudaLineHight(height) {
+    let paragraphs = document.querySelectorAll('.paragraph');
+    for (let i = 0; i < paragraphs.length; i += 1) {
+      paragraphs[i].style.lineHeight = height;
+    }
+    localStorage.setItem('lineHeight', height);
+  }
+
+  let lineHeightButtons = document.querySelectorAll('#line-height>button');
+  for (let i = 0; i < lineHeightButtons.length; i += 1) {
+    lineHeightButtons[i].addEventListener('click', function(event) {
+      mudaLineHight(event.target.innerHTML);
+    })
+  }
+
+  // Font Family
+  function mudaFontFamily(family) {
+    let paragraphs = document.querySelectorAll('.paragraph');
+    for (let i = 0; i < paragraphs.length; i += 1) {
+      paragraphs[i].style.fontFamily = family;
+    }
+    localStorage.setItem('fontFamily', family);
+  }
+
+  let fontFamilyButtons = document.querySelectorAll('#font-family>button');
+  for (let i = 0; i < fontFamilyButtons.length; i += 1) {
+    fontFamilyButtons[i].addEventListener('click', function(event) {
+      mudaFontFamily(event.target.innerHTML);
+    })
+  }
+}
