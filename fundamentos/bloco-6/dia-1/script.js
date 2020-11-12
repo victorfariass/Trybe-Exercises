28c66f5b5829eb7574420cbd7b1e2d6dcd6037f1
victor Farias <victorfarias.tv@gmail.com>
2020-11-12 13:49:08 -0300

    Adicionado estados de forma dinamica pelo js

---
+++ fundamentos/bloco-6/dia-1/script.js
@@ -0,0 +1,9 @@
+function adcionaEstados(estados) {
+  let container = document.getElementById('container');
+  for (let i = 0; i < estados.length; i += 1) {
+    estadoItem = document.createElement('option');
+    estadoItem.innerText = estados[i];
+    container.appendChild(estadoItem);
+  }
+}
+adcionaEstados(['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins'])
