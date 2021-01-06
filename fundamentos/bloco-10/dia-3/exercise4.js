907ebf594d6bc36c44a6ec2364b911edd5a3931d
victor Farias <victorfarias.tv@gmail.com>
2021-01-06 16:17:49 -0300

    Adicionado exercicios do 1 ao 4

---
+++ fundamentos/bloco-10/dia-3/exercise4.js
@@ -0,0 +1,9 @@
+const function1 = (str) => str.toUpperCase();
+const function2 = (str) => str.charAt(0);
+const function3 = (str1, str2) => str1.concat(str2);
+
+module.exports = {
+  function1,
+  function2,
+  function3,
+}
