b7a92bb061c931afad3c851b9e9f10a1fd1d3c92
victor Farias <victorfarias.tv@gmail.com>
2021-04-15 15:20:38 -0300

    Adicionados exercicios 21.1

---
+++ desenvolvimento-backend/bloco-21/dia-1/fixacao_01.sql
@@ -0,0 +1,29 @@
+SELECT UCASE('Oi, eu sou uma string');
+SELECT LCASE('Oi, seu sou uma string');
+SELECT REPLACE('Oi, eu sou uma string', 'string', 'cadeia de caracteres');
+SELECT LEFT('Oi, eu sou uma string', 3);
+SELECT RIGHT('Oi, eu sou uma string', 6);
+SELECT LENGTH('Oi, eu sou uma string');
+SELECT SUBSTRING('Oi, eu sou uma string', 5, 2);
+SELECT SUBSTRING('Oi, eu sou uma string', 5);
+SELECT UCASE(title) FROM sakila.film LIMIT 10;
+SELECT LCASE(title) FROM sakila.film LIMIT 10;
+SELECT REPLACE(title, 'ACADEMY', 'FOO') FROM sakila.film WHERE film_id = 1;
+SELECT LEFT(title, 7) FROM sakila.film WHERE film_id = 1;
+SELECT RIGHT(title, 8) FROM sakila.film WHERE film_id = 1;
+SELECT LENGTH(title) FROM sakila.film WHERE film_id = 1;
+SELECT SUBSTRING(title, 5, 2) FROM sakila.film WHERE film_id = 1;
+SELECT SUBSTRING(title, 5) FROM sakila.film WHERE film_id = 1;
+
+-- Exercícios de Fixação
+
+-- 1. Faça uma query que exiba a palavra 'trybe' em CAIXA ALTA.
+SELECT UCASE('trybe');
+-- 2. Faça uma query que transforme a frase 'Você já ouviu falar do DuckDuckGo?' em 'Você já ouviu falar do Google?' .
+SELECT REPLACE('Você já ouviu falar do DuckDuckGo?', 'DuckDuckGo', 'Google');
+-- 3. Utilizando uma query , encontre quantos caracteres temos em 'Uma frase qualquer' .
+SELECT LENGTH('Uma frase qualquer');
+-- 4. Extraia e retorne a palavra "JavaScript" da frase 'A linguagem JavaScript está entre as mais usadas' .
+SELECT SUBSTRING('A linguagem JavaScript está entre as mais usadas', 12, 11);
+-- 5. Por fim, padronize a string 'RUA NORTE 1500, SÃO PAULO, BRASIL' para que suas informações estejam todas em caixa baixa.
+SELECT LCASE('RUA NORTE1500, SÃO PAULO, BRASIL');
